71a451726e92b36f7de280f2f0234f017dbb6463
Kosuke Kurosawa <hk361117kk@akane.waseda.jp>
2019-01-16 07:24:01 +0000

    “init”

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,9 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require rails-ujs
-//= require jquery
+//= require jquery_ujs 
 //= require bootstrap/dist/js/bootstrap.min
+//= require rails-ujs
 //= bootstrap-sprockets
 //= require turbolinks
 //= require_tree .
