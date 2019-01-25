867de8da888745aafed546950050c48f26465769
Kosuke Kurosawa <hk361117kk@akane.waseda.jp>
2019-01-25 06:00:09 +0000

    “application.js変更”

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,7 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery_ujs
+//= require rails-ujs
 //= require bootstrap/dist/js/bootstrap.min
 //= require rails-ujs
 //= bootstrap-sprockets
